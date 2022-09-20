172a695e532d1c007477a5bf250e0b4ebd312e33
Александр Косилов <ted7007@yandex.ru>
2022-09-20 20:43:03 +1000

    first commit

--- HTTPApiTemplate/Program.cs
+++ HTTPApiTemplate/Program.cs
@@ -1,6 +1,27 @@
+using HTTPApiTemplate.Models;
+using HTTPApiTemplate.Repository;
+using HTTPApiTemplate.Repository.Argument;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddSingleton<IRepository<Product, CreateProductArgument, UpdateProductArgument>, ProductService>();
+builder.Services.AddControllers();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapControllers();
+app.MapGet("/", () => $"{app.Environment.EnvironmentName}");
 
 app.Run();
+
+/*  todo
+ 
+ * global logging
+ * global exception handler
+ * authorization & authentification
+ * automapper
+ * Data Access Layer(?) Unity repo?
+ 
+ 
+*/
+
